a183385b8a72b5920643c08c04fe95e7ef8f71b3
yoanybejarano <yoanybejarano@gmail.com>
2026-05-04 20:48:04 -0600

    integration testing junit

--- src/main/java/io/hatefulbug/library/producer/config/AutoCreateConfig.java
+++ src/main/java/io/hatefulbug/library/producer/config/AutoCreateConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
+import static io.hatefulbug.library.producer.util.TopicsRepo.LIBRARY_TOPIC;
+
 @Configuration
 public class AutoCreateConfig {
 
     @Bean
     public NewTopic newTopic() {
-        return TopicBuilder.name("library_events")
+        return TopicBuilder.name(LIBRARY_TOPIC)
                 .partitions(1)
                 .replicas(1)
                 .build();
